1e7c8969c2a06c144ec03c7a037e4da3314f6ac5
pohlem <pohle.maxime@gmail.com>
2018-09-25 16:15:57 +0200

    human-machine interface adjustment
    Change of the menu.

--- functionsCV.js
+++ functionsCV.js
@@ -1,11 +1,17 @@
 function showOneDiv(anID) {
 	var myMenus = document.querySelectorAll('.my-menus'),
-	i = 0,
-	l = myMenus.length;
-
+	i = 0, l = myMenus.length;
 	for (i; i < l; i++) {
 		myMenus[i].style.display = 'none';
 	}
 	var div = document.getElementById(anID);
 	div.style.display = "block";
+}
+function showHideDiv(anID) {
+	var div = document.getElementById(anID);
+	if(div.style.display=="none"){
+		div.style.display = "block";
+	} else {
+		div.style.display="none";
+	}
 }
