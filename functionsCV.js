a4cd38555d9ca5cd1be0c122b4994443db00fc84
pohlem <pohle.maxime@gmail.com>
2018-09-26 12:15:03 +0200

    Add Bootstrap and change of the presentation
    Change the menu into a menu icon.
    Change of the presentation, header is removed.

--- functionsCV.js
+++ functionsCV.js
@@ -1,17 +1,16 @@
 function showOneDiv(anID) {
+	/*Shows one div and hide others*/
 	var myMenus = document.querySelectorAll('.my-menus'),
 	i = 0, l = myMenus.length;
 	for (i; i < l; i++) {
 		myMenus[i].style.display = 'none';
 	}
-	var div = document.getElementById(anID);
+	var div = document.getElementById('div'+anID);
 	div.style.display = "block";
-}
-function showHideDiv(anID) {
-	var div = document.getElementById(anID);
-	if(div.style.display=="none"){
-		div.style.display = "block";
-	} else {
-		div.style.display="none";
+	/*Color the menu a who matches to the div selected*/
+	var myNavA = document.querySelectorAll('.my-nav-a'), i = 0, l = myNavA.length;
+	for (i; i < l; i++) {
+		myNavA[i].style.backgroundColor = '#354458';
 	}
+	myNavA[anID-1].style.backgroundColor = '#080808';
 }
